d99c280eb45b154d52bd1a129caeba299e23a9b9
monikkacha <monikkacha@gmail.com>
2022-07-07 07:36:12 +0530

    contracts added

--- contracts/GoverananceToken.sol
+++ contracts/GoverananceToken.sol
@@ -3,35 +3,32 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes {
+contract GovernanceToken is ERC20Votes {
+    uint256 public s_maxSupply = 1000000000000000000000000;
+
     constructor(string memory name_, string memory symbol_)
         ERC20(name_, symbol_)
         ERC20Permit("GovernanceToken")
-    {}
-
-    // The functions below are overrides required by Solidity.
+    {
+        _mint(msg.sender, s_maxSupply);
+    }
 
     function _afterTokenTransfer(
         address from,
         address to,
         uint256 amount
-    ) internal override(ERC20, ERC20Votes) {
+    ) internal override(ERC20Votes) {
         super._afterTokenTransfer(from, to, amount);
     }
 
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
         super._mint(to, amount);
     }
 
     function _burn(address account, uint256 amount)
         internal
-        override(ERC20, ERC20Votes)
+        override(ERC20Votes)
     {
         super._burn(account, amount);
     }
