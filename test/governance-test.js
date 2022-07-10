7c5b42d546fb337da5b18d5b3ca35e0cfa900ea7
monikkacha <monikkacha@gmail.com>
2022-07-10 12:45:01 +0530

    governance proposal , vote , queue and execute flow completed

--- test/governance-test.js
+++ test/governance-test.js
@@ -19,14 +19,19 @@ describe("Governance", function () {
     let executorRole;
     let adminRole;
     let firstSigner;
+    let anotherAccount;
+    let generatedProposalId;
 
     async function deployGovernanceLockFixture() {
         firstSigner = await ethers.getSigner();
+        allAccounts = await ethers.getSigners();
 
         // Deploy GovernorToken Contract
         const GovernanceToken = await hre.ethers.getContractFactory("GovernanceToken");
         const governanceToken = await GovernanceToken.deploy(TOKEN_NAME, TOKEN_SYMOBOL);
         await governanceToken.deployed();
+        const delegateTx = await governanceToken.delegate(firstSigner.address);
+        await delegateTx.wait(1);
 
         // Deploy TimeLock Contract
         const TimeLock = await hre.ethers.getContractFactory("TimeLock");
@@ -74,6 +79,11 @@ describe("Governance", function () {
         expect(balance).to.equal(ethers.utils.parseEther('1000000'));
     });
 
+    it("can only be changed through governance", async () => {
+        const { box } = await loadFixture(deployGovernanceLockFixture);
+        await expect(box.store(55)).to.be.revertedWith("Ownable: caller is not the owner");
+    })
+
     it("Creeate Proposal", async function () {
         const { myGovernance, box } = await loadFixture(deployGovernanceLockFixture);
         const encodedFunction = await box.interface.encodeFunctionData(FUNCTION, [NEW_VALUE]);
@@ -83,17 +93,93 @@ describe("Governance", function () {
             [encodedFunction],
             PROPOSE_DESCRIPTION
         );
+
+        await moveBlocks(GOVERNANCE_DELAY + 1);
+
         const proposeReceipt = await proposeTx.wait(1);
         const proposalId = proposeReceipt.events[0].args.proposalId;
-        console.log('proposalId : ', proposalId);
+
         // check for event emitted from the smart contract
         expect(proposeReceipt.events[0].event).to.equal('ProposalCreated');
 
-        // await time.increaseTo(GOVERNANCE_DELAY + 1);
-        await network.provider.send("evm_increaseTime", [GOVERNANCE_DELAY + 1])
-        await network.provider.send("evm_mine")
+        // const proposalState = await myGovernance.state(proposalId);
+        // console.log('ProposalState :', proposalState);
 
-        const proposalState = await myGovernance.state(proposalId);
-        console.log('ProposalState :', proposalState);
+        generatedProposalId = proposalId;
     });
+
+    it("Vote on proposal with tokens", async () => {
+        const { myGovernance } = await loadFixture(deployGovernanceLockFixture);
+        const voteWay = 1;
+        const reason = 'I like the idea';
+        const voteTx = await myGovernance.castVoteWithReason(generatedProposalId, voteWay, reason);
+        const voteReceipt = await voteTx.wait(1);
+        expect(voteReceipt.events[0].event).to.equal('VoteCast');
+
+        // const proposalState = await myGovernance.state(generatedProposalId);
+        // console.log('proposalState ', proposalState);
+
+        await moveBlocks(GOVERNANCE_PERIOD + 1);
+    })
+
+
+    it("Check for box value", async () => {
+        const { box } = await loadFixture(deployGovernanceLockFixture);
+        const value = await box.retrieve();
+        expect(ethers.utils.formatEther(value)).to.equal('0.0');
+    });
+
+    it("Queue action", async () => {
+
+        const { myGovernance, box } = await loadFixture(deployGovernanceLockFixture);
+        // const proposalState = await myGovernance.state(generatedProposalId);
+        // console.log('proposalState ', proposalState);
+
+        const encodedFunction = await box.interface.encodeFunctionData(FUNCTION, [NEW_VALUE]);
+        const descriptionHash = ethers.utils.id(PROPOSE_DESCRIPTION)
+        const queueTx = await myGovernance.queue(
+            [box.address],
+            [0],
+            [encodedFunction],
+            descriptionHash
+        );
+        const queueReceipt = await queueTx.wait(1);
+        expect(queueReceipt.events[1].event).to.equal('ProposalQueued');
+        await moveTime(MIN_DELAY + 1)
+        await moveBlocks(1)
+    })
+
+    it("Execute action", async () => {
+        const { myGovernance, box } = await loadFixture(deployGovernanceLockFixture);
+        const encodedFunction = await box.interface.encodeFunctionData(FUNCTION, [NEW_VALUE]);
+        const descriptionHash = ethers.utils.id(PROPOSE_DESCRIPTION)
+        const executeTx = await myGovernance.execute(
+            [box.address],
+            [0],
+            [encodedFunction],
+            descriptionHash
+        );
+        const executeReceipt = await executeTx.wait(1);
+        expect(executeReceipt.events[0].event).to.equal('ProposalExecuted');
+    })
+
+    it("Check for box value after proposal", async () => {
+        const { box } = await loadFixture(deployGovernanceLockFixture);
+        const value = await box.retrieve();
+        expect(value).to.equal('10');
+    });
+
+    async function moveBlocks(timeToBeDelayed) {
+        for (let index = 0; index < timeToBeDelayed; index++) {
+            await network.provider.request({
+                method: "evm_mine",
+                params: [],
+            })
+        }
+    }
+
+    async function moveTime(amount) {
+        await network.provider.send("evm_increaseTime", [amount])
+    }
 });
+
